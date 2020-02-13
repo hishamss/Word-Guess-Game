78f3b71d6d8169854f39c3cfa089fa555ddcd8b3
hishamss <hishamss90@gmail.com>
2020-02-13 12:58:56 -0700

    created for loop to add dashes on the page represent the number of letters of the picked word.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,12 +2,20 @@ var game = {
   zoo_animals: ["owl", "cat", "bear", "gorilla", "tiger"],
   animal_data: { owl: ["owl.jpg", "owl.mp3"], cat: ["cat.jpg", "cat.mp3"] }
 };
+var word;
 
 function computer_choice() {
   var rand = Math.floor(Math.random() * game.zoo_animals.length);
-  console.log(game.zoo_animals[rand]);
+  word = game.zoo_animals[rand];
 }
 
-$(document).keyup(function() {
+$(document).ready(function() {
   computer_choice();
+  $("#curr-word").text("");
+  for (i = 0; i < word.length; i++) {
+    $("#curr-word").append("<p><strong>_</strong></p>");
+  }
 });
+
+// listen to any key pressed to start the game
+$(document).keyup(function() {});
