244636154ad23e61cf7635d8df08b9c011b28505
hishamss <hishamss90@gmail.com>
2020-02-13 20:34:09 -0700

    the page now listen to the pressed key and display the key on the page if it's in the word choosed by the computer

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 var game = {
-  zoo_animals: ["owl", "cat", "bear", "gorilla", "tiger"],
+  zoo_animals: ["OWL", "CAT", "BEAR", "GORILLA", "TIGER"],
   animal_data: { owl: ["owl.jpg", "owl.mp3"], cat: ["cat.jpg", "cat.mp3"] }
 };
 var word;
@@ -13,9 +13,18 @@ $(document).ready(function() {
   computer_choice();
   $("#curr-word").text("");
   for (i = 0; i < word.length; i++) {
-    $("#curr-word").append("<p><strong>_</strong></p>");
+    $("#curr-word").append("<p id='p" + i + "'" + "><strong>_</strong></p>");
   }
 });
 
-// listen to any key pressed to start the game
-$(document).keyup(function() {});
+// listen to any key pressed
+$(document).keyup(function(event) {
+  var key = String.fromCharCode(event.which);
+  var match_index = 0;
+  //   this loop will keep search in for the pressed key on word till nothing is found
+  while (word.indexOf(key, match_index) != -1) {
+    match_index = word.indexOf(key, match_index);
+    $("#p" + match_index).text(key);
+    match_index++;
+  }
+});
