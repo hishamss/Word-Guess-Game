899f56ac9a090ffc239ab47f895c47884294b6e3
hishamss <hishamss90@gmail.com>
2020-02-17 15:43:37 -0700

    orgnaized my functions in the game object

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,3 @@
-var game = {
-  zoo_animals: ["ZEBRA", "MONKEY", "LION", "ELEPHANT", "TIGER"],
-  animal_data: {
-    ZEBRA: ["zebra.mp3", "zebra.jpg"],
-    MONKEY: ["monkey.mp3", "monkey.jpg"],
-    LION: ["lion.mp3", "lion.jpg"],
-    ELEPHANT: ["elephant.mp3", "elephant.jpg"],
-    TIGER: ["tiger.mp3", "tiger.jpg"]
-  }
-};
 var word,
   guessed,
   attempts,
@@ -17,17 +7,46 @@ var word,
   CalculatedBodyHeight,
   animal_sound,
   win = 0;
-function computer_choice() {
-  var rand = Math.floor(Math.random() * game.zoo_animals.length);
-  word = game.zoo_animals[rand];
-}
+var game = {
+  zoo_animals: ["ZEBRA", "MONKEY", "LION", "ELEPHANT", "TIGER"],
+  animal_data: {
+    ZEBRA: ["zebra.mp3", "zebra.jpg"],
+    MONKEY: ["monkey.mp3", "monkey.jpg"],
+    LION: ["lion.mp3", "lion.jpg"],
+    ELEPHANT: ["elephant.mp3", "elephant.jpg"],
+    TIGER: ["tiger.mp3", "tiger.jpg"]
+  },
+  computer_choice: function() {
+    var rand = Math.floor(Math.random() * this.zoo_animals.length);
+    word = this.zoo_animals[rand];
+  },
+  // this function will be called after the page load or the user guess the whole word
+  random_pick: function() {
+    this.computer_choice();
+    $("#curr-word").text("");
+    for (i = 0; i < word.length; i++) {
+      $("#curr-word").append(
+        "<button class='btn btn-primary btn-lg dashes' id='b" +
+          i +
+          "'" +
+          "></button>"
+      );
+    }
+    guessed = 0;
+    alreadyguessed = [];
+    // the user can try the lenght of the picked word plus 4 to guess
+    attempts = word.length + 4;
+    $("#attempts").text(attempts);
+    $("#win").text(win);
+    $("#failed").text("");
+  }
+};
 
 $(document).ready(function() {
   $(".main").hide();
   $(".welcom").hide();
   $(".game_over").hide();
   $("#start").show();
-  // $("#answer").text("Let's Go Wild!!");
   $("#answer").append(
     "<img src='assets/images/guess.jpg' class='img-thumbnail img-fluid rounded'>"
   );
@@ -42,7 +61,8 @@ $(document).ready(function() {
   $(".welcom").css("margin-top", CalculatedBodyHeight1);
   $(".main").css("margin-top", CalculatedBodyHeight2);
   $(".game_over").css("margin-top", CalculatedBodyHeight3);
-  random();
+  // random();
+  game.random_pick();
   $("#start-btn").click(function() {
     $("#start").hide();
     $(".welcom").show();
@@ -55,87 +75,66 @@ $(document).ready(function() {
   $("#play_again-btn").click(function() {
     location.replace("index.html");
   });
-});
 
-// this function will be called after the page load or the user guess the whole word
-function random() {
-  computer_choice();
-  $("#curr-word").text("");
-  for (i = 0; i < word.length; i++) {
-    // $("#curr-word").append("<p id='p" + i + "'" + "><strong>_</strong></p>");
-    $("#curr-word").append(
-      "<button class='btn btn-primary btn-lg dashes' id='b" +
-        i +
-        "'" +
-        "></button>"
-    );
-  }
-  guessed = 0;
-  alreadyguessed = [];
-  //   the user can try the lenght of the picked word plus 4 to guess
-  attempts = word.length + 4;
-  $("#attempts").text(attempts);
-  $("#win").text(win);
-  $("#failed").text("");
-}
-// listen to any key pressed and check if the pressed key is in the word picked randomlly.
-$(document).keyup(function(event) {
-  var key = String.fromCharCode(event.which);
-  match_index = 0;
-  //   check if the pressed key is already guessed
-  if (!alreadyguessed.includes(key)) {
-    //   this loop will keep search in for the pressed key on word till nothing is found
-    while (word.indexOf(key, match_index) != -1) {
-      match_index = word.indexOf(key, match_index);
-      $("#b" + match_index).text(key);
-      match_index++;
-      guessed++;
-      alreadyguessed.push(key);
+  // listen to any key pressed and check if the pressed key is in the word picked randomlly.
+  $(document).keyup(function(event) {
+    var key = String.fromCharCode(event.which);
+    match_index = 0;
+    //   check if the pressed key is already guessed
+    if (!alreadyguessed.includes(key)) {
+      //   this loop will keep search in for the pressed key on word till nothing is found
+      while (word.indexOf(key, match_index) != -1) {
+        match_index = word.indexOf(key, match_index);
+        $("#b" + match_index).text(key);
+        match_index++;
+        guessed++;
+        alreadyguessed.push(key);
+      }
     }
-  }
 
-  if (word.indexOf(key) == -1) {
-    attempts--;
-    $("#attempts").text(attempts);
-    $("#failed").append("<p>" + key + ", </p>");
-  }
-  //   Game Over
-  if (attempts == 0) {
-    $(".main").hide();
-    $(".game_over").show();
-  }
+    if (word.indexOf(key) == -1) {
+      attempts--;
+      $("#attempts").text(attempts);
+      $("#failed").append("<p>" + key + ", </p>");
+    }
+    //   Game Over
+    if (attempts == 0) {
+      $(".main").hide();
+      $(".game_over").show();
+    }
 
-  //   if the user guessed the whole word
-  if (guessed === word.length) {
-    win++;
-    // THIS WILL STOP THE PRVIOUS VOICE FROM PLAYING
-    animal_sound.pause();
-    // this will play the animal sound based on the cuessed word
-    animal_sound = document.getElementById(game.animal_data[word][0]);
-    console.log(animal_sound);
-    animal_sound.play();
-    // this will display the word after the user guess it
-    $("#answer").text("");
-    $("#answer").append(
-      "<img src='assets/images/" +
-        game.animal_data[word][1] +
-        "' class='img-thumbnail img-fluid rounded'>"
-    );
-    // dilay 300 ms before call random() function
-    setTimeout(random, 300);
+    //   if the user guessed the whole word
+    if (guessed === word.length) {
+      win++;
+      // THIS WILL STOP THE PRVIOUS VOICE FROM PLAYING
+      animal_sound.pause();
+      // this will play the animal sound based on the cuessed word
+      animal_sound = document.getElementById(game.animal_data[word][0]);
+      console.log(animal_sound);
+      animal_sound.play();
+      // this will display the word after the user guess it
+      $("#answer").text("");
+      $("#answer").append(
+        "<img src='assets/images/" +
+          game.animal_data[word][1] +
+          "' class='img-thumbnail img-fluid rounded'>"
+      );
+      // dilay 300 ms before call random() function
+      setTimeout(game.random_pick(), 300);
+    }
+  });
+
+  window.addEventListener("resize", reposition);
+  // to keep start button alway in the middle of the screen
+  function reposition() {
+    BodyHeight = $("body").height();
+    CalculatedBodyHeight = BodyHeight * 0.5 - 60;
+    $("#start").css("margin-top", CalculatedBodyHeight);
+    CalculatedBodyHeight1 = BodyHeight * 0.5 - 225;
+    $(".welcom").css("margin-top", CalculatedBodyHeight1);
+    CalculatedBodyHeight2 = BodyHeight * 0.5 - 101;
+    $(".main").css("margin-top", CalculatedBodyHeight2);
+    CalculatedBodyHeight3 = BodyHeight * 0.5 - 92;
+    $(".game_over").css("margin-top", CalculatedBodyHeight3);
   }
 });
-
-window.addEventListener("resize", reposition);
-// to keep start button alway in the middle of the screen
-function reposition() {
-  BodyHeight = $("body").height();
-  CalculatedBodyHeight = BodyHeight * 0.5 - 60;
-  $("#start").css("margin-top", CalculatedBodyHeight);
-  CalculatedBodyHeight1 = BodyHeight * 0.5 - 225;
-  $(".welcom").css("margin-top", CalculatedBodyHeight1);
-  CalculatedBodyHeight2 = BodyHeight * 0.5 - 101;
-  $(".main").css("margin-top", CalculatedBodyHeight2);
-  CalculatedBodyHeight3 = BodyHeight * 0.5 - 92;
-  $(".game_over").css("margin-top", CalculatedBodyHeight3);
-}
